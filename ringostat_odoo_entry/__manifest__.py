5ccb785d2de4ae0608b72afda0579c37c967808b
MariannaMary <m.popkova@ringostat.com>
2025-05-23 13:39:53 +0300

    Add files via upload

---
+++ ringostat_odoo_entry/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Ringostat",
+    "summary": "Integration with Ringostat  AI-Powered Business Phone & Marketing Performance Platform",
+    "description": "This module serves as the entry point for integrating with the Ringostat platform an AI-powered business phone, call tracking, and analytics platform.\n\nDisplays a page with integration details.\nAdds a button that redirects users to their Ringostat account.\nServes as an informational entry point for the SaaS platform.",
+    "author": "Ringostat Ukraine",
+    "website": "https://ringostat.com/integrations/",
+    "category": "Industries",
+    "version": "1.0",
+    "depends": ["base"],
+    "data": ["views/template.xml"],
+    "images": ["static/description/screenshot.gif"],
+    "installable": False,
+    "application": False,
+    "license": "OPL-1",
+    "live_test_url": "https://ringostat.com/integrations/",
+    "price": 1.00,
+    "currency": "EUR"
+}
